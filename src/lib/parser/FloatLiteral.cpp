766d4d33de4125d41eeeb7f6f6fd653639e50a17
LuisHsu <luishsuplus@gmail.com>
2020-04-11 04:58:51 +0800

    Parse hex float literal

--- src/lib/parser/FloatLiteral.cpp
+++ src/lib/parser/FloatLiteral.cpp
@@ -28,7 +28,7 @@ static void decimalNumber(std::any& thisAny, ParserContext& context, bool negati
     }
     ++context.cursor;
   }
-  // Decimal point
+  // Point
   if((*context.cursor) != '.'){
     thisAny = (negative) ? -value : value;
     return;
@@ -81,44 +81,97 @@ static void decimalNumber(std::any& thisAny, ParserContext& context, bool negati
 }
 
 static void hexNumber(std::any& thisAny, ParserContext& context, bool negative){
-  // FIXME:
-  /*
+  double value = 0;
+  // Integer part
   if((*context.cursor) != '_'){
-    while (context.cursor != context.end){
+    while(context.cursor != context.end){
       if((*context.cursor) >= '0' && (*context.cursor) <= '9'){
-        if(!thisAny.has_value()){
-          thisAny = (int64_t)((*context.cursor) - '0');
-        }else{
-          thisAny = (std::any_cast<int64_t>(thisAny) << 4) + (*context.cursor) - '0';
-        }
+        value = value * 16 + (*context.cursor) - '0';
       }else if((*context.cursor) >= 'a' && (*context.cursor) <= 'f'){
-        if(!thisAny.has_value()){
-          thisAny = (int64_t)(((*context.cursor) - 'a' + 10) * (isNegative ? -1 : 1));
-        }else{
-          thisAny = (std::any_cast<int64_t>(thisAny) << 4) + (*context.cursor) - 'a' + 10;
-        }
+        value = value * 16 + (*context.cursor) - 'a' + 10;
       }else if((*context.cursor) >= 'A' && (*context.cursor) <= 'F'){
-        if(!thisAny.has_value()){
-          thisAny = (int64_t)(((*context.cursor) - 'A' + 10) * (isNegative ? -1 : 1));
-        }else{
-          thisAny = (std::any_cast<int64_t>(thisAny) << 4) + (*context.cursor) - 'A' + 10;
-        }
-      }else if((*context.cursor) != '_' || context.cursor + 1 == context.end || (
-        (context.cursor[1] < '0' || context.cursor[1] > '9') &&
-        (context.cursor[1] < 'a' || context.cursor[1] > 'f') &&
-        (context.cursor[1] < 'A' || context.cursor[1] > 'F'))
+        value = value * 16 + (*context.cursor) - 'A' + 10;
+      }else if(
+        (*context.cursor) != '_' ||
+        context.cursor + 1 == context.end || (
+          (context.cursor[1] < '0' ||  context.cursor[1] > '9') &&
+          (context.cursor[1] < 'a' ||  context.cursor[1] > 'f') &&
+          (context.cursor[1] < 'A' ||  context.cursor[1] > 'F')
+        )
       ){
         if((*context.cursor) == '_'){
-          thisAny.reset();
+          return;
         }
         break;
       }
       ++context.cursor;
     }
-    if(thisAny.has_value() && isNegative){
-      thisAny = -std::any_cast<int64_t>(thisAny);
+  }else{
+    return;
+  }
+  // Point
+  if((*context.cursor) != '.'){
+    thisAny = (negative) ? -value : value;
+    return;
+  }else{
+    ++context.cursor;
+  }
+  // fragment part
+  if((*context.cursor) != '_'){
+    for(double exp = -1; context.cursor != context.end;){
+      if((*context.cursor) >= '0' && (*context.cursor) <= '9'){
+        value += pow(16, exp) * ((*context.cursor) - '0');
+        --exp;
+      }else if((*context.cursor) >= 'a' && (*context.cursor) <= 'f'){
+        value += pow(16, exp) * ((*context.cursor) - 'a' + 10);
+        --exp;
+      }else if((*context.cursor) >= 'A' && (*context.cursor) <= 'F'){
+        value += pow(16, exp) * ((*context.cursor) - 'A' + 10);
+        --exp;
+      }else if(
+        (*context.cursor) != '_' ||
+        context.cursor + 1 == context.end || (
+          (context.cursor[1] < '0' ||  context.cursor[1] > '9') &&
+          (context.cursor[1] < 'a' ||  context.cursor[1] > 'f') &&
+          (context.cursor[1] < 'A' ||  context.cursor[1] > 'F')
+        )
+      ){
+        if((*context.cursor) == '_'){
+          return;
+        }
+        break;
+      }
+      ++context.cursor;
     }
-  }*/
+  }else{
+    return;
+  }
+  // Expoment part
+  if((*context.cursor) == 'P' || (*context.cursor) == 'p'){
+    ++context.cursor;
+    bool exponent_negative = isNegative(context);
+    double exponent = 0;
+    if((*context.cursor) != '_'){
+      while(context.cursor != context.end){
+        if((*context.cursor) >= '0' && (*context.cursor) <= '9'){
+          exponent = exponent * 10 + (*context.cursor) - '0';
+        }else if((*context.cursor) != '_' || context.cursor + 1 == context.end || context.cursor[1] < '0' || context.cursor[1] > '9'){
+          if((*context.cursor) == '_'){
+            return;
+          }
+          break;
+        }
+        ++context.cursor;
+      }
+      exponent = (exponent_negative) ? -exponent : exponent;
+      value *= pow(16, exponent);
+      thisAny = (negative) ? -value : value;
+    }else{
+      return;
+    }
+  }else{
+    thisAny = (negative) ? -value : value;
+  }
 }
 
 static void nanNumber(std::any& thisAny, ParserContext& context, bool negative){
--- test/unittests/lib/parser/test_FloatLiteral.cpp
+++ test/unittests/lib/parser/test_FloatLiteral.cpp
@@ -347,3 +347,244 @@ TEST(unittest_FloatLiteral, decimal_seperator_postfix_exponent){
   EXPECT_EQ(context.cursor, data.begin());
   EXPECT_FALSE(result.has_value());
 }
+
+TEST(unittest_FloatLiteral, hex_integer_only){
+  std::vector<char> data(create_char_vector("0x2e"));
+  ParserContext context(data);
+  FloatLiteral result(context);
+  EXPECT_EQ(context.cursor, data.end());
+  EXPECT_TRUE(result.has_value());
+  EXPECT_FLOAT_EQ(std::any_cast<double>(result), 0x2e);
+}
+
+TEST(unittest_FloatLiteral, hex_integer_with_point){
+  std::vector<char> data(create_char_vector("0x2e."));
+  ParserContext context(data);
+  FloatLiteral result(context);
+  EXPECT_EQ(context.cursor, data.end());
+  EXPECT_TRUE(result.has_value());
+  EXPECT_FLOAT_EQ(std::any_cast<double>(result), 0x2e);
+}
+
+TEST(unittest_FloatLiteral, hex_regular){
+  std::vector<char> data(create_char_vector("0x1C.4"));
+  ParserContext context(data);
+  FloatLiteral result(context);
+  EXPECT_EQ(context.cursor, data.end());
+  EXPECT_TRUE(result.has_value());
+  EXPECT_FLOAT_EQ(std::any_cast<double>(result), 28.25);
+}
+
+TEST(unittest_FloatLiteral, plus_hex){
+  std::vector<char> data(create_char_vector("+0x1C.4"));
+  ParserContext context(data);
+  FloatLiteral result(context);
+  EXPECT_EQ(context.cursor, data.end());
+  EXPECT_TRUE(result.has_value());
+  EXPECT_FLOAT_EQ(std::any_cast<double>(result), 28.25);
+}
+
+TEST(unittest_FloatLiteral, minus_hex){
+  std::vector<char> data(create_char_vector("-0x1C.4"));
+  ParserContext context(data);
+  FloatLiteral result(context);
+  EXPECT_EQ(context.cursor, data.end());
+  EXPECT_TRUE(result.has_value());
+  EXPECT_FLOAT_EQ(std::any_cast<double>(result), -28.25);
+}
+
+TEST(unittest_FloatLiteral, hex_seperator_integer){
+  std::vector<char> data(create_char_vector("0x1_C.08"));
+  ParserContext context(data);
+  FloatLiteral result(context);
+  EXPECT_EQ(context.cursor, data.end());
+  EXPECT_TRUE(result.has_value());
+  EXPECT_FLOAT_EQ(std::any_cast<double>(result), 28.03125);
+}
+
+TEST(unittest_FloatLiteral, hex_seperator_integer_more){
+  std::vector<char> data(create_char_vector("0x1_2_0.08"));
+  ParserContext context(data);
+  FloatLiteral result(context);
+  EXPECT_EQ(context.cursor, data.end());
+  EXPECT_TRUE(result.has_value());
+  EXPECT_FLOAT_EQ(std::any_cast<double>(result), 288.03125);
+}
+
+TEST(unittest_FloatLiteral, hex_seperator_fraction){
+  std::vector<char> data(create_char_vector("0x1C.0_8"));
+  ParserContext context(data);
+  FloatLiteral result(context);
+  EXPECT_EQ(context.cursor, data.end());
+  EXPECT_TRUE(result.has_value());
+  EXPECT_FLOAT_EQ(std::any_cast<double>(result), 28.03125);
+}
+
+TEST(unittest_FloatLiteral, hex_seperator_fraction_more){
+  std::vector<char> data(create_char_vector("0x1C.0_8_0"));
+  ParserContext context(data);
+  FloatLiteral result(context);
+  EXPECT_EQ(context.cursor, data.end());
+  EXPECT_TRUE(result.has_value());
+  EXPECT_FLOAT_EQ(std::any_cast<double>(result), 28.03125);
+}
+
+TEST(unittest_FloatLiteral, hex_seperator_mix){
+  std::vector<char> data(create_char_vector("0x1_2.0_80"));
+  ParserContext context(data);
+  FloatLiteral result(context);
+  EXPECT_EQ(context.cursor, data.end());
+  EXPECT_TRUE(result.has_value());
+  EXPECT_FLOAT_EQ(std::any_cast<double>(result), 18.03125);
+}
+
+TEST(unittest_FloatLiteral, hex_seperator_mix_more){
+  std::vector<char> data(create_char_vector("0x0_20_0.0_80_0"));
+  ParserContext context(data);
+  FloatLiteral result(context);
+  EXPECT_EQ(context.cursor, data.end());
+  EXPECT_TRUE(result.has_value());
+  EXPECT_FLOAT_EQ(std::any_cast<double>(result), 512.03125);
+}
+
+TEST(unittest_FloatLiteral, hex_seperator_prefix_plus){
+  std::vector<char> data(create_char_vector("+0x_42.2a"));
+  ParserContext context(data);
+  FloatLiteral result(context);
+  EXPECT_EQ(context.cursor, data.begin());
+  EXPECT_FALSE(result.has_value());
+}
+
+TEST(unittest_FloatLiteral, hex_seperator_prefix_minus){
+  std::vector<char> data(create_char_vector("-0x_42.2b"));
+  ParserContext context(data);
+  FloatLiteral result(context);
+  EXPECT_EQ(context.cursor, data.begin());
+  EXPECT_FALSE(result.has_value());
+}
+
+TEST(unittest_FloatLiteral, hex_seperator_prefix_integer){
+  std::vector<char> data(create_char_vector("0x_42.2c"));
+  ParserContext context(data);
+  FloatLiteral result(context);
+  EXPECT_EQ(context.cursor, data.begin());
+  EXPECT_FALSE(result.has_value());
+}
+
+TEST(unittest_FloatLiteral, hex_seperator_double_integer){
+  std::vector<char> data(create_char_vector("0x4__2.d5"));
+  ParserContext context(data);
+  FloatLiteral result(context);
+  EXPECT_EQ(context.cursor, data.begin());
+  EXPECT_FALSE(result.has_value());
+}
+
+TEST(unittest_FloatLiteral, hex_seperator_postfix_integer){
+  std::vector<char> data(create_char_vector("0x42_.a5"));
+  ParserContext context(data);
+  FloatLiteral result(context);
+  EXPECT_EQ(context.cursor, data.begin());
+  EXPECT_FALSE(result.has_value());
+}
+
+TEST(unittest_FloatLiteral, hex_seperator_prefix_fraction){
+  std::vector<char> data(create_char_vector("0x42._15"));
+  ParserContext context(data);
+  FloatLiteral result(context);
+  EXPECT_EQ(context.cursor, data.begin());
+  EXPECT_FALSE(result.has_value());
+}
+
+TEST(unittest_FloatLiteral, hex_seperator_double_fraction){
+  std::vector<char> data(create_char_vector("0x42.2__5"));
+  ParserContext context(data);
+  FloatLiteral result(context);
+  EXPECT_EQ(context.cursor, data.begin());
+  EXPECT_FALSE(result.has_value());
+}
+
+TEST(unittest_FloatLiteral, hex_seperator_postfix_fraction){
+  std::vector<char> data(create_char_vector("0x42.25_"));
+  ParserContext context(data);
+  FloatLiteral result(context);
+  EXPECT_EQ(context.cursor, data.begin());
+  EXPECT_FALSE(result.has_value());
+}
+
+TEST(unittest_FloatLiteral, hex_exponent){
+  std::vector<char> data(create_char_vector("0x22.008p2"));
+  ParserContext context(data);
+  FloatLiteral result(context);
+  EXPECT_EQ(context.cursor, data.end());
+  EXPECT_TRUE(result.has_value());
+  EXPECT_FLOAT_EQ(std::any_cast<double>(result), 8704.5);
+}
+
+TEST(unittest_FloatLiteral, hex_upper_exponent){
+  std::vector<char> data(create_char_vector("0x22.008P2"));
+  ParserContext context(data);
+  FloatLiteral result(context);
+  EXPECT_EQ(context.cursor, data.end());
+  EXPECT_TRUE(result.has_value());
+  EXPECT_FLOAT_EQ(std::any_cast<double>(result), 8704.5);
+}
+
+TEST(unittest_FloatLiteral, hex_plus_exponent){
+  std::vector<char> data(create_char_vector("0x22.008p+2"));
+  ParserContext context(data);
+  FloatLiteral result(context);
+  EXPECT_EQ(context.cursor, data.end());
+  EXPECT_TRUE(result.has_value());
+  EXPECT_FLOAT_EQ(std::any_cast<double>(result), 8704.5);
+}
+
+TEST(unittest_FloatLiteral, hex_minus_exponent){
+  std::vector<char> data(create_char_vector("0x12.8p-1"));
+  ParserContext context(data);
+  FloatLiteral result(context);
+  EXPECT_EQ(context.cursor, data.end());
+  EXPECT_TRUE(result.has_value());
+  EXPECT_FLOAT_EQ(std::any_cast<double>(result), 1.15625);
+}
+
+TEST(unittest_FloatLiteral, hex_exponent_separator){
+  std::vector<char> data(create_char_vector("0x12.8p-0_1"));
+  ParserContext context(data);
+  FloatLiteral result(context);
+  EXPECT_EQ(context.cursor, data.end());
+  EXPECT_TRUE(result.has_value());
+  EXPECT_FLOAT_EQ(std::any_cast<double>(result), 1.15625);
+}
+
+TEST(unittest_FloatLiteral, hex_exponent_separator_more){
+  std::vector<char> data(create_char_vector("0x12.08p+0_0_1"));
+  ParserContext context(data);
+  FloatLiteral result(context);
+  EXPECT_EQ(context.cursor, data.end());
+  EXPECT_TRUE(result.has_value());
+  EXPECT_FLOAT_EQ(std::any_cast<double>(result), 288.5);
+}
+
+TEST(unittest_FloatLiteral, hex_seperator_prefix_exponent){
+  std::vector<char> data(create_char_vector("0x42.2p_25"));
+  ParserContext context(data);
+  FloatLiteral result(context);
+  EXPECT_EQ(context.cursor, data.begin());
+  EXPECT_FALSE(result.has_value());
+}
+
+TEST(unittest_FloatLiteral, hex_seperator_double_exponent){
+  std::vector<char> data(create_char_vector("0x42.7p2__5"));
+  ParserContext context(data);
+  FloatLiteral result(context);
+  EXPECT_EQ(context.cursor, data.begin());
+  EXPECT_FALSE(result.has_value());
+}
+
+TEST(unittest_FloatLiteral, hex_seperator_postfix_exponent){
+  std::vector<char> data(create_char_vector("0x42.8p25_"));
+  ParserContext context(data);
+  FloatLiteral result(context);
+  EXPECT_EQ(context.cursor, data.begin());
+  EXPECT_FALSE(result.has_value());
+}
