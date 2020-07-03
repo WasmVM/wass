e8ff3eb98e5bd2e6e4ebe63b1dda2738783766db
LuisHsu <luishsuplus@gmail.com>
2020-07-04 02:37:07 +0800

    Generate Global

---
+++ test/unittests/lib/codegen/test_GenGlobal.cpp
@@ -0,0 +1,27 @@
+#include <gtest/gtest.h>
+
+#include <variant>
+#include <Error.hpp>
+#include <structure/Global.hpp>
+#include <structure/InstrVariant.hpp>
+#include <structure/ConstInstr.hpp>
+#include <codegen/CodeGenVisitor.hpp>
+#include <Helper.hpp>
+
+TEST(unittest_GenGlobal, const_value){
+  Global data;
+  data.globalType.immutable = true;
+  data.globalType.type = ValueType::i32;
+  data.expr.emplace<I32ConstInstr>().value = 5;
+  CodeGenVisitor visitor;
+  EXPECT_EQ(std::visit<BinaryCode>(visitor, CodeGenVariant(data)), BinaryCode({'\x7F', '\x00', '\x41', '\x05'}));
+}
+
+TEST(unittest_GenGlobal, mutable_value){
+  Global data;
+  data.globalType.immutable = false;
+  data.globalType.type = ValueType::i32;
+  data.expr.emplace<GlobalGetInstr>().index = (uint32_t) 7;
+  CodeGenVisitor visitor;
+  EXPECT_EQ(std::visit<BinaryCode>(visitor, CodeGenVariant(data)), BinaryCode({'\x7F', '\x01', '\x23', '\x07'}));
+}
